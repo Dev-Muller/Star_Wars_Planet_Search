aa7f98d33c349800419a0c43a392e76638c16ed2
muller <pedromullergl11@gmail.com>
2023-05-07 13:27:47 -0300

    test: implemented tests for 30% of the app

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from '../App';
+import AppProvider from '../context/AppProvider';
 import mockData from './helpers/mockData';
 
-test('Teste a página da aplicacao', () => {
+describe('Teste a página da aplicacao', () => {
   beforeEach(() => {
     jest.spyOn(global, 'fetch').mockResolvedValue({
       json: () => Promise.resolve(
@@ -17,7 +19,6 @@ test('Teste a página da aplicacao', () => {
     );
   });
   it('Verifica se inputs e table estão presentes', ()=> {
-    
     const textInput = screen.getByTestId('name-filter');
     const columnFilter = screen.getByTestId('column-filter');
     const comparisonFilter = screen.getByTestId('comparison-filter');
@@ -64,6 +65,11 @@ test('Teste a página da aplicacao', () => {
       name: /url/i,
     });
 
+    const orderSort = screen.getByTestId('column-sort');
+    const asc = screen.getByTestId('column-sort-input-asc');
+    const desc = screen.getByTestId('column-sort-input-desc');
+    const sortBtn = screen.getByTestId('column-sort-button');
+
     expect(textInput).toBeInTheDocument();
     expect(columnFilter).toBeInTheDocument();
     expect(comparisonFilter).toBeInTheDocument();
@@ -83,6 +89,129 @@ test('Teste a página da aplicacao', () => {
     expect(tableHeadCreated).toBeInTheDocument();
     expect(tableHeadEdited).toBeInTheDocument();
     expect(tableHeadUrl).toBeInTheDocument();
+    expect(orderSort).toBeInTheDocument();
+    expect(asc).toBeInTheDocument();
+    expect(desc).toBeInTheDocument();
+    expect(sortBtn).toBeInTheDocument();
+    expect(columnFilter.children).toHaveLength(5);
+    expect(comparisonFilter.children).toHaveLength(3);
+    expect(orderSort.children).toHaveLength(5);
   });
+  it('testa inputs de filtro igual a e textInput', async () => {
+    const textInput = screen.getByTestId('name-filter');
+    const columnFilter = screen.getByTestId('column-filter');
+    const comparisonFilter = screen.getByTestId('comparison-filter');
+    const valueFilter = screen.getByTestId('value-filter');
+    const filterBtn = screen.getByTestId('button-filter');
+    
+    await waitFor(() => {
+      const table = screen.getByRole('table');
+      const cell = table.querySelectorAll('tr')
+      expect(cell).toHaveLength(11);
+    });
 
+    userEvent.type(textInput, 'too')
+
+    await waitFor(() => {
+      const table = screen.getByRole('table');
+      const cell = table.querySelectorAll('tr')
+      expect(cell).toHaveLength(2);
+      expect(screen.getByRole('cell', {
+        name: /tatooine/i,
+      })).toBeInTheDocument();
+    });
+
+    userEvent.clear(textInput);
+
+    userEvent.selectOptions(columnFilter, 'rotation_period');
+    userEvent.selectOptions(comparisonFilter, 'igual a')
+    userEvent.type(valueFilter, 27);
+    userEvent.click(filterBtn);
+
+    await waitFor(() => {
+      const showFilter = screen.getByTestId('filter');
+  
+      expect(showFilter).toBeInTheDocument();
+    });
+
+    await waitFor(() => {
+      const columnFilter = screen.getByTestId('column-filter');
+      expect(columnFilter.children).toHaveLength(4);
+    });
+  });
+  it('testa inputs de filtro maior que', async () => {
+    const columnFilter = screen.getByTestId('column-filter');
+    const comparisonFilter = screen.getByTestId('comparison-filter');
+    const valueFilter = screen.getByTestId('value-filter');
+    const filterBtn = screen.getByTestId('button-filter');
+    
+    await waitFor(() => {
+      const table = screen.getByRole('table');
+      const cell = table.querySelectorAll('tr')
+      expect(cell).toHaveLength(11);
+    });
+
+    userEvent.selectOptions(columnFilter, 'rotation_period');
+    userEvent.selectOptions(comparisonFilter, 'maior que')
+    userEvent.type(valueFilter, 26);
+    userEvent.click(filterBtn);
+
+    await waitFor(() => {
+      const showFilter = screen.getByTestId('filter');
+  
+      expect(showFilter).toBeInTheDocument();
+    });
+  });
+  it('testa inputs de filtro menor que', async () => {
+    const columnFilter = screen.getByTestId('column-filter');
+    const comparisonFilter = screen.getByTestId('comparison-filter');
+    const valueFilter = screen.getByTestId('value-filter');
+    const filterBtn = screen.getByTestId('button-filter');
+    
+    await waitFor(() => {
+      const table = screen.getByRole('table');
+      const cell = table.querySelectorAll('tr')
+      expect(cell).toHaveLength(11);
+    });
+
+    userEvent.selectOptions(columnFilter, 'rotation_period');
+    userEvent.selectOptions(comparisonFilter, 'menor que')
+    userEvent.type(valueFilter, 13);
+    userEvent.click(filterBtn);
+
+    await waitFor(() => {
+      const showFilter = screen.getByTestId('filter');
+  
+      expect(showFilter).toBeInTheDocument();
+    });
+  });
+  it('testa os botao de excluir apenas um filtro', async () => {
+    const columnFilter = screen.getByTestId('column-filter');
+    const comparisonFilter = screen.getByTestId('comparison-filter');
+    const valueFilter = screen.getByTestId('value-filter');
+    const filterBtn = screen.getByTestId('button-filter');
+    
+    await waitFor(() => {
+      const table = screen.getByRole('table');
+      const cell = table.querySelectorAll('tr')
+      expect(cell).toHaveLength(11);
+    });
+
+    userEvent.selectOptions(columnFilter, 'rotation_period');
+    userEvent.selectOptions(comparisonFilter, 'menor que')
+    userEvent.type(valueFilter, 13);
+    userEvent.click(filterBtn);
+
+    await waitFor(() => {
+      const showFilter = screen.getByTestId('filter');
+      const removeOneFilter = screen.getByRole('button', {
+        name: /x/i,
+      });
+      const removeAllFilters = screen.getByTestId('button-remove-filters');
+      expect(removeOneFilter).toBeInTheDocument();
+      expect(removeAllFilters).toBeInTheDocument();
+      userEvent.click(removeOneFilter);
+      expect(showFilter).not.toBeInTheDocument();
+    });
+  });
 });
